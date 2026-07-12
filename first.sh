0f2a19a8a0c75c5e4d699fd94a3666687c739e20
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2026-07-12 22:49:02 +0600

    information

--- first.sh
+++ first.sh
@@ -1,1 +1,13 @@
-mv new.sh first.sh
+# pwd -- show current directory
+# ls -la - show list file
+# cd  - change directory 
+#  mkdir -- create a new folder - mkdir new.sh
+# touch -- create a new file - touch newww.sh
+# rm -- delete a file  rm new.sh
+# cp -- copy a file  - cp a.sh b.sh
+# mv -- move a file  -- mv new.sh old.sh
+#cat -- display file contents -- eat new.sh
+# echo -- print --
+# why -n  -- Do not print a newline after the output - echo -n "Hello "
+#echo "World"  output-- Hello World
+
